b809b560aee705fb2f8b2179b3c087f5d0dee1b5
FEDeveloperSoftflux <graphicdesignerteam442@gmail.com>
2026-09-14 17:28:41 +0500

    pushed

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-// src/api/index.js
+// src/api/index.js file
 import axios from "axios";
 export const baseUrl = "https://api.sprintexa.com/api/";
 //export const baseUrl = "https://gknmkz56-5000.inc1.devtunnels.ms/api/";
