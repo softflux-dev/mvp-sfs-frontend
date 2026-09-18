c08dfb992c34a50ae8ea88699d078703de716b65
softflux-dev <qatesting552@gmail.com>
2026-09-18 15:46:54 +0500

    neww revert

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MainLayout from "./components/layout";
 
 function App() {
   return (
-    <BrowserRouter basename="/login">
+    <BrowserRouter>
       <Routes>
 
         {/* ── Auth routes (redirect to "/" if already logged in) ─────────── */}
